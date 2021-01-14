dac57fcbc2189bf2f65981c26ba7d35063cd4a32
Igor Gomes <igorgmsf@gmail.com>
2021-01-13 21:47:51 -0300

    exemplos de children e composição

--- learning-props/src/app.js
+++ learning-props/src/app.js
@@ -1,17 +1,19 @@
 "use strict";
 
 import React, { Component } from "react";
-// import Title from "./title";
-import Square from "./square";
-
+import Button from "./button";
+import LikeButton from "./like-button";
+import SearchButton from "./search-button";
 class App extends Component {
   render() {
     return (
       <div className="container">
-        {["blue", "red", "green", "yellow"].map((square) => (
-          // a key é ncessária para que o react possa saber se o elemento que está sendo passado no array é NavigationPreloadManager, ou se é preciso destruir o que existia anteriormente, caso o elemento seja o mesmo o DOM sabe que não precisa modificá-lo
-          <Square key={square} color={square} />
-        ))}
+        <Button>
+          <span>Texto</span>
+          outro texto
+        </Button>
+        <LikeButton />
+        <SearchButton />
       </div>
     );
   }
---
+++ learning-props/src/search-button.js
@@ -0,0 +1,8 @@
+"use strict";
+
+import React from "react";
+import Button from "./button";
+
+const SearchButton = () => <Button>Search</Button>;
+
+export default SearchButton;
